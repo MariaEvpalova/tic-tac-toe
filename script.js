f0e7d1d9616e0efe6d4d497a594d4d17f60d8d35
MariaEvpalova <mashayevpalova@gmail.com>
2023-05-25 16:17:17 +0530

    Add classes and modules - Refine game structure by adding classes and modules and interation between them - Add check on tie in the end of the game

--- script.js
+++ script.js
@@ -1,53 +1,117 @@
-const cells = document.querySelectorAll('.cell');
-let currentPlayer = 'X';
+// Factory function for players
+const Player = (name, symbol) => {
+    return { name, symbol };
+};
 
-cells.forEach((cell) => {
-    cell.addEventListener('click', handleCellClick);
-});
+// Module for gameboard
+const Gameboard = (() => {
+    let board = ['', '', '', '', '', '', '', '', ''];
 
-function handleCellClick(e) {
-    const clickedCell = e.target;
+    const getBoard = () => board;
 
-    if (clickedCell.textContent !== '') return;
+    const resetBoard = () => {
+        board = ['', '', '', '', '', '', '', '', ''];
+    };
 
-    clickedCell.textContent = currentPlayer;
-    clickedCell.classList.add(currentPlayer);
+    const placeMark = (index, symbol) => {
+        if (board[index] === '') {
+            board[index] = symbol;
+            return true;
+        }
+        return false;
+    };
 
-    if (checkWin()) {
-        alert(`Player ${currentPlayer} wins!`);
-        resetBoard();
-        return;
+    return { getBoard, resetBoard, placeMark };
+})();
+
+// Module for display controller
+const DisplayController = (() => {
+    const gameboardElement = document.getElementById('game-board');
+    const resetButton = document.getElementById('reset-btn');
+
+    resetButton.addEventListener('click', resetGame);
+
+    function resetGame() {
+        Gameboard.resetBoard();
+        renderBoard();
     }
 
-    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-}
+    function renderBoard() {
+        const board = Gameboard.getBoard();
 
-function checkWin() {
-    const winningCombinations = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8], // rows
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8], // columns
-        [0, 4, 8],
-        [2, 4, 6], // diagonals
-    ];
+        gameboardElement.innerHTML = '';
 
-    return winningCombinations.some((combination) => {
-        return (
-            cells[combination[0]].textContent === currentPlayer &&
-            cells[combination[1]].textContent === currentPlayer &&
-            cells[combination[2]].textContent === currentPlayer
-        );
-    });
-}
+        board.forEach((symbol, index) => {
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            cell.textContent = symbol;
+            cell.addEventListener('click', () => handleCellClick(index));
 
-function resetBoard() {
-    cells.forEach((cell) => {
-        cell.textContent = '';
-        cell.classList.remove('X', 'O');
-    });
+            gameboardElement.appendChild(cell);
+        });
+    }
 
-    currentPlayer = 'X';
-}
+    function handleCellClick(index) {
+        const currentPlayer = GameController.getCurrentPlayer();
+
+        if (Gameboard.placeMark(index, currentPlayer.symbol)) {
+            renderBoard();
+
+            if (GameController.checkWin(currentPlayer)) {
+                alert(`Player ${currentPlayer.name} wins!`);
+                resetGame();
+            } else if (GameController.checkTie()) {
+                alert("It's a tie!");
+                resetGame();
+            } else {
+                GameController.switchPlayer();
+            }
+        }
+    }
+
+    return { renderBoard };
+})();
+
+// Module for game controller
+const GameController = (() => {
+    const player1 = Player('Player 1', 'X');
+    const player2 = Player('Player 2', 'O');
+    let currentPlayer = player1;
+
+    const getCurrentPlayer = () => currentPlayer;
+
+    const switchPlayer = () => {
+        currentPlayer = currentPlayer === player1 ? player2 : player1;
+    };
+
+    const checkWin = (player) => {
+        const board = Gameboard.getBoard();
+        const winningCombinations = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8], // rows
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8], // columns
+            [0, 4, 8],
+            [2, 4, 6], // diagonals
+        ];
+
+        return winningCombinations.some((combination) => {
+            return (
+                board[combination[0]] === player.symbol &&
+                board[combination[1]] === player.symbol &&
+                board[combination[2]] === player.symbol
+            );
+        });
+    };
+
+    const checkTie = () => {
+        const board = Gameboard.getBoard();
+        return board.every((cell) => cell !== '');
+    };
+
+    return { getCurrentPlayer, switchPlayer, checkWin, checkTie };
+})();
+
+DisplayController.renderBoard();
